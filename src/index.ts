f8c3dbe269fcc3af637f8cfa6d78ec8cb2d127a8
laoer536 <1547698569@qq.com>
2023-07-02 15:49:02 +0800

    feat: getNeededInfoFromCssTree

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { cwd } from 'node:process'
 import { join } from 'node:path'
-import { readFileCode, getLessCssCode, getCssTree } from './utils'
+import { readFileCode, getLessCssCode, getNeededInfoFromCssTree } from './utils'
 async function run() {
   const lessCode = readFileCode(join(cwd(), './test/less/react/index.module.less'))
-  console.log(lessCode)
   const cssCode = await getLessCssCode(lessCode)
   console.log(cssCode)
-  console.log(getCssTree(cssCode))
+  console.log(getNeededInfoFromCssTree(cssCode))
 }
 
 run().then(() => {
--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ import less from 'less'
 import sass from 'sass'
 import { readFileSync } from 'node:fs'
 import * as csstree from 'css-tree'
+import type { ParseOptions } from 'css-tree'
+import type { AtrulePrelude } from 'css-tree'
+import { AtrulePlain, AtrulePreludePlain, ClassSelector, TypeSelector } from 'css-tree'
+
 export function getStyleFileType(fileName: string) {
   const nameArr = fileName.split('.')
   const suffix = nameArr.pop()
@@ -32,6 +36,25 @@ export function readFileCode(filePath: string) {
   return readFileSync(filePath, 'utf-8')
 }
 
-export function getCssTree(cssCode: string) {
-  return csstree.parse(cssCode)
+export function getCssTree(cssCode: string, options?: ParseOptions) {
+  return csstree.parse(cssCode, options) as AtrulePrelude
+}
+
+export function getNeededInfoFromCssTree(cssCode: string) {
+  const cssArrHasAll: string[][] = []
+  const info1Arr = getCssTree(cssCode).children.toArray() as AtrulePlain[]
+  for (const info1 of info1Arr) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const info2Arr = (info1.prelude as AtrulePreludePlain).children.toArray() as AtrulePrelude[]
+    for (const info2 of info2Arr) {
+      const cssList = info2.children.toArray() as Array<ClassSelector | TypeSelector>
+      cssArrHasAll.push(
+        cssList
+          .filter((info3) => ['ClassSelector', 'TypeSelector'].includes(info3.type))
+          .map((info4) => (info4.type === 'ClassSelector' ? `.${info4.name}` : info4.name))
+      )
+    }
+  }
+  return cssArrHasAll
 }
