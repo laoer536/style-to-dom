15aa7b3bfe281ec014a4fa23cb871c9dbdb22299
laoer536 <1547698569@qq.com>
2023-12-21 20:29:28 +0800

    feat: user code style select start

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,21 +2,32 @@ import { useState } from 'react'
 import './App.scss'
 import { MdEditor } from 'md-editor-rt'
 import 'md-editor-rt/lib/style.css'
+import { defaultCode } from './utils/data.ts'
 
 function InputCard() {
+  const [textareaCode, setTextareaCode] = useState(defaultCode)
   return (
     <div className={'input-card'}>
       <div className={'title'}>Please enter your style code</div>
-      <textarea spellCheck={false} autoFocus={true} />
+      <textarea value={textareaCode} spellCheck={false} autoFocus={true} />
     </div>
   )
+}
+
+function ResultCard() {
+  return <div className={'result-card'}>ResultCard</div>
 }
 
 function App() {
   const [text, setText] = useState('# Hello Editor')
   const [isShowEdit, setIsShowEdit] = useState(false)
 
-  return <div className="app">{isShowEdit ? <MdEditor modelValue={text} onChange={setText} /> : <InputCard />}</div>
+  return (
+    <div className="app">
+      <ResultCard />
+      {isShowEdit ? <MdEditor modelValue={text} onChange={setText} /> : <InputCard />}
+    </div>
+  )
 }
 
 export default App
--- web/src/utils/data.ts
+++ web/src/utils/data.ts
@@ -21,3 +21,40 @@ export enum Parser {
   vue = 'vue',
   html = 'html',
 }
+
+export const defaultCode = `.input-card {
+    width: 60%;
+    height: 70%;
+    flex-shrink: 0;
+    border-radius: 21.44px;
+    background: rgba(217, 217, 217, 0.31);
+    backdrop-filter: blur(10px);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    padding: 20px;
+    .title {
+      color: #fff;
+      font-size: 29.829px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      text-align: center;
+    }
+    textarea {
+      width: 100%;
+      margin-top: 20px;
+      background: transparent;
+      text-align: left;
+      height: calc(100% - 76px);
+      padding: 10px 20px;
+      border: 1px solid white;
+      outline-color: white;
+      font-size: 16px;
+      @extend .title;
+      &:hover {
+        cursor: url('/cursor-text.svg'), text;
+      }
+    }
+  }`
